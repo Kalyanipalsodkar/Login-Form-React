b87b9937d3507327103a57aafae0e0bdb17820d1
Kalyanipalsodkar <138213118+Kalyanipalsodkar@users.noreply.github.com>
2024-01-29 15:00:19 +0530

    App.js

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
+import Todoform from './Components/Todoform';
 
-function App() {
-  return (
-    <div className="App">
-      <Login />  
+function App(){
+  return(
+    <div className='App'>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/Todoform' element={<Todoform />} />
+        </Routes>
     </div>
   );
 }
